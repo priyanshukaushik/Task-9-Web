38a3eeaee95b469ae593feb84009581bfd4556fc
priyanshukaushik <84342622+priyanshukaushik@users.noreply.github.com>
2021-09-01 21:31:17 +0530

    Add files via upload

---
+++ Task-9-master/index.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+// fs.mkdirSync("Priyanshu Kaushik");
+// fs.writeFileSync("task.txt","I am Priyanshu , Member of ACM society")
+fs.appendFileSync("task.txt",". I am going to be a great coder for sure. ")
+// const data = fs.readFileSync("task.txt","utf-8");
+// console.log(data);
+// fs.renameSync("task.txt", "task9.txt");
+// fs.unlinkSync("task9.txt");
